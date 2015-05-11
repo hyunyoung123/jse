3b5d6d60c2ca90ecae7f03b12b687efe9565ee4c
dongjin <dongjinmedia@gmail.com>
2015-05-11 11:12:44 +0900

    launching..

---
+++ jse/src/cmm01/var/var.java
@@ -0,0 +1,28 @@
+package cmm01.var;
+
+public class var { 
+	public static void main(String[] args) {
+		 /* 
+		  * 변수 종류 
+		  * 문자타입 : char
+		  * 숫자타입 : (정수) : byte, short, int, long
+		  * 숫자타입 : (실수) : float , double
+		  * 논리 타입 : boolean
+		  * */
+		int x; // 변수선언
+		
+		x=1; // 할당 연산자를 사용한 값을 할당 ( 또는 대입 )
+		
+		int y;
+		
+		y = x+1;
+		
+		System.out.println("x+1의 값" + y);
+		/*
+		 * "" 안에 문자입력해서 문자출력  
+		 * 변수 출력하려면 "" 없어야함 ㅇㅇ 
+		 * ""랑 변수를 같이쓸라면 "문자" 입력후 + 변수명
+		 * */
+	}
+} 
+
