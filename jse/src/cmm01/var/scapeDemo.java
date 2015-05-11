7dc87b39c3af42fd7d0fd1987c3194d587dcc418
dongjin <dongjinmedia@gmail.com>
2015-05-11 11:55:31 +0900

    escape study..

---
+++ jse/src/cmm01/var/scapeDemo.java
@@ -0,0 +1,18 @@
+package cmm01.var;
+
+public class scapeDemo {
+	public static void main(String[] args) {
+		int kor = 90 ;
+				int eng = 85 ; 
+				int sum = 0;
+						
+						sum = kor + eng ;
+				
+				System.out.print("\t성적처리\n");
+				System.out.println("==============");
+				System.out.println("국어+\t+ 영어 \t+총점");
+				System.out.println(kor +"\t" + eng "\t" +  sum);
+		
+	}
+
+}
